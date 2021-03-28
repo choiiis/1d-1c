252b9c428a393fa769ff6bb8e6f4e846afa49fed
choiiis <haemin0128@naver.com>
2021-03-29 00:01:30 +0900

    20210328 solved problems

--- programmers_완주하지못한선수.cpp
+++ programmers_완주하지못한선수.cpp
@@ -10,9 +10,10 @@ string solution(vector<string> participant, vector<string> completion) {
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
     
-    for(int i = 0; i < participant.size(); i++) {
+    for(int i = 0; i < completion.size(); i++) {
         if(participant[i] != completion[i])
             return participant[i];
     }
-    return answer;
+    // if not found, last participant is the answer
+    return participant[participant.size() - 1];
 }
